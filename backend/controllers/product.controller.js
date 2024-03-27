314331b9f9556ecc00bdefb5b017435e62cd49b9
Ashu Negi <anegi6723@gmail.com>
2024-03-27 12:53:02 +0530

    created updated product controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,14 +1,45 @@
 const Product = require("../models/product.model");
 
-exports.createProduct = async (req,res,next) => {
+// Create Product --Admin
+exports.createProduct = async (req, res, next) => {
     let product = await Product.create(req.body);
     res.status(201).json({
-        success : true,
+        success: true,
         product
     })
 }
 
-exports.getAllProducts = (req,res) => {
-    res.status(200).json({msg : "Root is working fine"});
+// Get all Products
+exports.getAllProducts = async (req, res) => {
+    let products = await Product.find({});
+    res.status(200).json({
+        success: true,
+        products
+    });
 }
 
+// Update product -- Admin
+exports.updateProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            msg: "Product not found"
+        })
+    }
+
+    try{
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }catch(err){
+        res.status(500).json({success : false, error : err})
+    }
+}
