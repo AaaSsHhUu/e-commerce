41799ba7426289b5725af356f4dd40d090c521a0
Ashu Negi <anegi6723@gmail.com>
2024-03-27 10:42:01 +0530

    set up routes and json middleware

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+//  Routes 
+app.use("/api/v1", require("./routes/product.route"));
+
 module.exports = app;
