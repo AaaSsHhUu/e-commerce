af9784108297ac5683dd74104d49d794222e5e57
Ashu Negi <anegi6723@gmail.com>
2024-03-27 12:53:25 +0530

    added updated product route and mdified prev route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/product.controller");
+const { getAllProducts, createProduct, updateProduct } = require("../controllers/product.controller");
 const router = express.Router();
 
-router.route("/product").get(getAllProducts)
+router.route("/products").get(getAllProducts)
 router.route("/product/new").post(createProduct)
+router.route("/product/:id").put(updateProduct)
 
 module.exports = router;
