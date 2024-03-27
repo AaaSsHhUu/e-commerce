9a1fcd1adb2bc7426451425112e2826e3a41dd0c
Ashu Negi <anegi6723@gmail.com>
2024-03-27 11:31:03 +0530

    fixed error

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,4 +2,6 @@ const express = require("express");
 const { getAllProducts } = require("../controllers/product.controller");
 const router = express.Router();
 
-router.route("/products",getAllProducts);
+router.route("/products").get(getAllProducts)
+
+module.exports = router;
