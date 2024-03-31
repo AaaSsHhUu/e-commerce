280d9a1521b4123a6af259030dc5e816afbae937
Ashu Negi <anegi6723@gmail.com>
2024-03-31 19:11:19 +0530

    handling error for wrong id

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err,req,res,next) => {
     err.status = err.status || 500;
     err.message = err.message  || "Internal server error";
 
+    // Mongodb Wrong id error
+    if(err.name === "CastError"){
+        const message = `Resource Not Found Invalid : ${err.path}`;
+        err = new ErrorHandler(400,message);
+    }
+
     res.status(err.status).json({
         success : false,
         message : err.message,
