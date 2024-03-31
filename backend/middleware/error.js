0e6d935fafc20ca222f5172ba6e07dd91beb4092
Ashu Negi <anegi6723@gmail.com>
2024-03-31 18:12:33 +0530

    created error middleware

---
+++ backend/middleware/error.js
@@ -0,0 +1,11 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err,req,res,next) => {
+    err.status = err.status || 500;
+    err.message = err.message  || "Internal server error";
+
+    res.status(err.status).json({
+        success : false,
+        error : err
+    })
+}
