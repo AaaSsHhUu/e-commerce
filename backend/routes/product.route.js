e9becd33bbf3716442380251d8b5dbe9c2c4803f
Ashu Negi <anegi6723@gmail.com>
2024-03-31 18:52:30 +0530

    used asyncHandler

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail } = require("../controllers/product.controller");
 const router = express.Router();
+const asyncHandler = require("../middleware/asyncHandler");
 
-router.route("/products").get(getAllProducts)
+router.route("/products").get(asyncHandler(getAllProducts))
 
-router.route("/product/new").post(createProduct)
+router.route("/product/new").post(asyncHandler(createProduct))
 
 router.route("/product/:id")
-    .get(getProductDetail)
-    .put(updateProduct)
-    .delete(deleteProduct)
+    .get(asyncHandler(getProductDetail))
+    .put(asyncHandler(updateProduct))
+    .delete(asyncHandler(deleteProduct))
 
 module.exports = router;
