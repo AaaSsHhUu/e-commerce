2d586878b6c0e3fc0cfa97851aa335c51976091b
Ashu Negi <anegi6723@gmail.com>
2024-03-31 18:13:57 +0530

    used errorMiddleware in app

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-
+const errorMiddleware = require("./middleware/error");
 app.use(express.json());
 
 //  Routes 
 app.use("/api/v1", require("./routes/product.route"));
+
+// Error handling
+app.use(errorMiddleware);
 
 module.exports = app;
