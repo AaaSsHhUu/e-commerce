69b75fa6cad86d7b8879cd4a256626ab8f00da0e
Ashu Negi <anegi6723@gmail.com>
2024-03-31 18:24:06 +0530

    modified

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-exports.connectDB = async() => {
-    await mongoose.connect(process.env.DB_URI)
+exports.connectDB = () => {
+    mongoose.connect(process.env.DB_URI)
     .then((data) => {
         console.log("Mongodb connected to server ", data.connection.host);
     }).catch((err) => {
