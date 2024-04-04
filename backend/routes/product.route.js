67d4ed016a3c1669e6fc71f533c2ff0d01873426
Ashu Negi <anegi6723@gmail.com>
2024-04-04 11:35:36 +0530

    used validateProduct in product route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail } = require("../controllers/product.controller");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
+const { validateUser, validateProduct } = require("../middleware/validation");
 
 router.route("/products").get(asyncHandler(getAllProducts))
 
-router.route("/product/new").post(asyncHandler(createProduct))
+router.route("/product/new").post(validateProduct,asyncHandler(createProduct))
 
 router.route("/product/:id")
     .get(asyncHandler(getProductDetail))
