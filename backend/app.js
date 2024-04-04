3c687a310fbeec94518d24339fe005c69edf74cf
Ashu Negi <anegi6723@gmail.com>
2024-04-04 12:08:20 +0530

    added user.route in app

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const errorMiddleware = require("./middleware/error");
 app.use(express.json());
 
 //  Routes 
-app.use("/api/v1", require("./routes/product.route"));
+app.use("/api/v1/", require("./routes/product.route"));
+app.use("/api/v1/", require("./routes/user.route"));
 
 // Error handling
 app.use(errorMiddleware);
