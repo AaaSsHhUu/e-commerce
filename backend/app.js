0fd4eeda5606caeec8821d76704ade2950c078b3
Ashu Negi <anegi6723@gmail.com>
2024-04-10 12:45:03 +0530

    added cookie parser middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
+const cookiParser = require("cookie-parser");
+
 app.use(express.json());
+app.use(cookiParser());
 
 //  Routes 
 app.use("/api/v1/", require("./routes/product.route"));
