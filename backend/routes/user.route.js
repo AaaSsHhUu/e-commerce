0d1f9d5193b5b93c3e34a30b8a9f09d22de9a8ee
Ashu Negi <anegi6723@gmail.com>
2024-04-10 12:46:12 +0530

    added logout route tin user routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
 const { validateUser } = require("../middleware/validation");
-const { registerUser, loginUser } = require('../controllers/user.controller');
+const { registerUser, loginUser, logoutUser } = require('../controllers/user.controller');
 
-router.route("/register").post(validateUser, asyncHandler(registerUser))
-router.route("/login").post(asyncHandler(loginUser))
+router.route("/register").post(validateUser, asyncHandler(registerUser));
+
+router.route("/login").post(asyncHandler(loginUser));
+
+router.route("/logout").get(logoutUser);
 
 module.exports = router;
