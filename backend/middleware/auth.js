c101b5b2b539d5384c1cfb7ecd93b23eaf88e5b1
Ashu Negi <anegi6723@gmail.com>
2024-04-10 13:48:10 +0530

    bug fix

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,16 +4,25 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 const isAuthenticated = asyncHandler(async(req,res,next) => {
-    const token = req.cookies;
+    const {token} = req.cookies;
     // console.log(token);
     if(!token){
         return new ErrorHandler(401,"Please Login first");
     }
 
     const decodedData = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET_KEY);
-    console.log("decoded data : ",decodedData);
+    // console.log("decoded data : ",decodedData);
     req.user = await User.findById(decodedData.id);
     next();
 })
 
-module.exports = isAuthenticated;
+const authorizeRoles = (...roles) => {
+    return (req,res,next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(403, `Role : ${req.user.role} is not allowed to access this resource`))
+        }
+        next();
+    }
+}
+
+module.exports = {isAuthenticated, authorizeRoles};
