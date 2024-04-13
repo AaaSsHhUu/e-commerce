683e511115fc09e3293e094998ca95ab4529ab08
Ashu Negi <anegi6723@gmail.com>
2024-04-13 19:58:55 +0530

    modified configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,10 +2,12 @@ const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodeMailer.createTransport({
+        // host : "smtp.gmail",
+        // port : 465, // This is alternate approach to use nodemailer without passing App_Password(from your account) in pass.
         service : process.env.SMTP_SERVICE,
         auth : {
             user : process.env.SMTP_MAIL,
-            password : process.env.SMTP_PASSWORD
+            pass : process.env.SMTP_APP_PASSWORD 
         }
     })
 
