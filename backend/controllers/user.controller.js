226317953553c1609996adf220c77ff7412a6d03
Ashu Negi <anegi6723@gmail.com>
2024-04-13 18:12:56 +0530

    added resetPassword controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorHandler");
 const User = require("../models/user.model");
 const sendToken = require('../utils/generateToken');
+const sendEmail = require("../utils/sendEmail")
 
 const registerUser = async (req,res,next)=>{
     let {name, email, password} = req.body;
@@ -45,4 +46,40 @@ const logoutUser = async (req,res) => {
         message : "Logged out successfully"
     })
 }
-module.exports = {registerUser, loginUser, logoutUser}
+
+const resetPassword = async(req,res,next) => {
+    const user = await User.findOne({email : req.body.email});
+
+    if(!user){
+        return next(new ErrorHandler(404, "User not found"))
+    }
+
+    // Get reset password token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({validateBeforeSave : false});
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested to reset your password, then please don't open it.`
+
+    try{
+        await sendEmail({
+            email : user.email,
+            subject : "E-commerce password recovery",
+            message
+        })
+
+        res.status(200).json({
+            success : true,
+            message : `Email sent to ${user.email} successfully`
+        })
+    }catch(err){
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        user.save({validateBeforeSave : false});
+
+        return next(new ErrorHandler(500, err.message))
+    }
+}
+module.exports = {registerUser, loginUser, logoutUser, resetPassword}
