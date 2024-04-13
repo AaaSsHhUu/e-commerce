eb544f1c318084429567756b955b7398d452658e
Ashu Negi <anegi6723@gmail.com>
2024-04-13 18:13:50 +0530

    added route for reset password

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
 const { validateUser } = require("../middleware/validation");
-const { registerUser, loginUser, logoutUser } = require('../controllers/user.controller');
+const { registerUser, loginUser, logoutUser, resetPassword } = require('../controllers/user.controller');
 
 router.route("/register").post(validateUser, asyncHandler(registerUser));
 
 router.route("/login").post(asyncHandler(loginUser));
 
 router.route("/logout").get(logoutUser);
+
+router.route("/password/reset").post(resetPassword)
 
 module.exports = router;
