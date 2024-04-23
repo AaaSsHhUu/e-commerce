e303175d0833882445a63badb320becde9f7ee42
Ashu Negi <anegi6723@gmail.com>
2024-04-23 19:51:44 +0530

    added route for deleteReview controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail, createProductReview, getProductReviews } = require("../controllers/product.controller");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail, createProductReview, getProductReviews, deleteReview } = require("../controllers/product.controller");
 const router = express.Router();
 const asyncHandler = require("../middleware/asyncHandler");
 const { validateProduct } = require("../middleware/validation");
 const { isAuthenticated, authorizeRoles } = require("../middleware/auth");
 
 
-router.route("/products").get(asyncHandler(getAllProducts))
+router.route("/products").get(asyncHandler(getAllProducts));
 
-router.route("/admin/product/new").post(isAuthenticated, authorizeRoles("admin"), validateProduct, asyncHandler(createProduct))
+router.route("/admin/product/new").post(isAuthenticated, authorizeRoles("admin"), validateProduct, asyncHandler(createProduct));
 
-router.route("/product/:id").get(asyncHandler(getProductDetail))
+router.route("/product/:id").get(asyncHandler(getProductDetail));
 
 router.route("/admin/product/:id")
     .put(isAuthenticated, authorizeRoles("admin"), asyncHandler(updateProduct))
-    .delete(isAuthenticated, authorizeRoles("admin"), asyncHandler(deleteProduct))
+    .delete(isAuthenticated, authorizeRoles("admin"), asyncHandler(deleteProduct));
 
 router.route("/review").put(isAuthenticated, asyncHandler(createProductReview));
-router.route("/reviews").get(getProductReviews)
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticated, asyncHandler(deleteReview));
 module.exports = router;
