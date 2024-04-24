92095149632f69449ad34bf547349d5a8e471123
Ashu Negi <anegi6723@gmail.com>
2024-04-24 12:11:25 +0530

    created oder schema

---
+++ backend/models/order.model.js
@@ -0,0 +1,107 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    shippingInfo : {
+        address : {
+            type : String,
+            required : true
+        },
+        city : {
+            type : String,
+            required : true
+        },
+        state : {
+            type : String,
+            required : true
+        },
+        country : {
+            type :String,
+            default : "India"
+        },
+        pinCode : {
+            type : Number,
+            required : true
+        },
+        phoneNo : {
+            type : Number,
+            required : true
+        }
+    },
+    orderItems : [
+        {
+            name : {
+                type : STring,
+                required : true
+            },
+            price : {
+                type : Number,
+                equired : true
+            },
+            quantity : {
+                type : Number,
+                required : true
+            },
+            image : {
+                type : String,
+                required : true
+            },
+            product : {
+                type : Schema.Types.ObjectId,
+                required : true
+            }
+        }
+    ],
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    },
+    paymentInfo : {
+        id : {
+            type : String,
+            required : true
+        },
+        status : {
+            type : String,
+            required : true
+        }
+    },
+    paidAt : {
+        type : Date,
+        required : true
+    },
+    itemsPrice : {
+        type : Number,
+        default : 0,
+        required : true
+    },
+    taxPrice : {
+        type : Number,
+        default : 0,
+        required : true
+    },
+    shippingPrice : {
+        type : Number,
+        default : 0,
+        required : true
+    },
+    totalPrice : {
+        type : Number,
+        default : 0,
+        required : true
+    },
+    orderStatus : {
+        type : String,
+        required : true,
+        default : "Processing"
+    },
+    deliveredAt : Date
+}, 
+{timestamps : true}
+)
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
+
