7a25a5f9261ad775428ed26390674489327be1b1
Ashu Negi <anegi6723@gmail.com>
2024-04-28 10:27:11 +0530

    added order route in app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ app.use(cookiParser());
 //  Routes 
 app.use("/api/v1/", require("./routes/product.route"));
 app.use("/api/v1/", require("./routes/user.route"));
+app.use("/api/v1/", require("./routes/order.route"));
 
 // Error handling
 app.use(errorMiddleware);
