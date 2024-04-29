92bbc293f1ab073fe4026dd55efe711f6fd750c4
Ashu Negi <anegi6723@gmail.com>
2024-04-29 10:14:29 +0530

    added route for getting current user orders

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {isAuthenticated, authorizeRoles} = require("../middleware/auth");
-const { newOrder, getSingleOrder } = require("../controllers/order.controller");
+const { newOrder, getSingleOrder, getMyOrders } = require("../controllers/order.controller");
 
 
 router.route("/order/new").post(isAuthenticated,newOrder);
 
 router.route("/order/:id").get(isAuthenticated, authorizeRoles("admin"), getSingleOrder);
+
+router.route("/order/me").get(isAuthenticated, getMyOrders);
 
 module.exports = router;
