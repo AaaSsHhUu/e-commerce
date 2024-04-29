01f64162a3dcec79c48a971347c05185d5c8d03f
Ashu Negi <anegi6723@gmail.com>
2024-04-29 10:12:47 +0530

    added route for getting single order by admin

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {isAuthenticated} = require("../middleware/auth");
-const { newOrder } = require("../controllers/order.controller");
+const {isAuthenticated, authorizeRoles} = require("../middleware/auth");
+const { newOrder, getSingleOrder } = require("../controllers/order.controller");
 
 
 router.route("/order/new").post(isAuthenticated,newOrder);
+
+router.route("/order/:id").get(isAuthenticated, authorizeRoles("admin"), getSingleOrder);
 
 module.exports = router;
