800e84a06f844ab650c2e3a17871935d9d1fa467
Ashu Negi <anegi6723@gmail.com>
2024-05-19 13:33:33 +0530

    added Header Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/layouts/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Header />
     </div>
   );
 }
