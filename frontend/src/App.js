7e351b208a81751f41d3bb265ddb6094a3d11e5e
Ashu Negi <anegi6723@gmail.com>
2024-05-20 18:58:34 +0530

    fetching google fonts using webfontloader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import WebFont from "webfontloader";
 
 function App() {
+  
+  // Fetching font at the start of rendering
+  useEffect(()=>{
+      WebFont.load({
+        google : {
+          families : ["Roboto","Droid Sans", "Chilanka"]
+        }
+      })
+  },[])
+
   return (
     <div className="App">
         <Outlet />
