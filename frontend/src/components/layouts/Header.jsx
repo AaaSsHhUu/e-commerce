1200ab67aa21987e012ea0e5cb2f4b7946cb8875
Ashu Negi <anegi6723@gmail.com>
2024-05-20 18:51:52 +0530

    create navbar using overlay-navbar package

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,8 +1,47 @@
-import { ReactNavbar } from 'overlay-navbar'
-import React from 'react'
+import React from 'react';
+import { ReactNavbar } from 'overlay-navbar';
+import { FaCartArrowDown, FaSearch, FaUserCircle } from "react-icons/fa";
+import { logo } from '../../assets';
 
 function Header() {
-  return <ReactNavbar />
+  return <ReactNavbar
+      burgerColor = "#eb4034"
+      burgerColorHover = "#a62d24"
+      logo = {logo}
+      logoWidth = "20vmax"
+      navColor1 = "rgba(0,0,0,0.4)"
+      logoHoverSize = "10px"
+      // logoHoverColor = "#eb4034"
+      searchIcon
+      cartIcon 
+      profileIcon
+      SearchIconElement = {FaSearch}
+      CartIconElement = {FaCartArrowDown}
+      ProfileIconElement = {FaUserCircle}
+      link1Text = "Home"
+      link2Text = "Product"
+      link3Text = "Contact"
+      link4Text = "About"
+      link1Url = "/"
+      link2Url = "/product"
+      link3Url = "/contact"
+      link4Url = "/about"
+      link1Size = "1.3vmax"
+      link1Color = "rgba(35,35,35,0.8)"
+      nav1justifyContent = "flex-end"
+      nav2justifyContent = "flex-end"
+      nav3justifyContent = "flex-start"
+      nav4justifyContent = "flex-start"
+      link1ColorHover = "#eb4034"
+      link1Margin = "1vmax"
+      profileIconColor = "rgba(35,35,35,0.8)"
+      cartIconColor = "rgba(35,35,35,0.8)"
+      searchIconColor = "rgba(35,35,35,0.8)"
+      profileIconColorHover = "#eb4034"
+      searchIconColorHover = "#eb4034"
+      cartIconColorHover = "#eb4034"
+      cartIconMargin = "1vmax"
+  />
 }
 
 export default Header
