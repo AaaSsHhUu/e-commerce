a8bf6c67810114e5c60d88465d2311f062d34643
Ashu Negi <anegi6723@gmail.com>
2024-05-22 11:10:57 +0530

    modified navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,26 +5,22 @@ import Sidebar from "./Sidebar";
 
 function Navbar() {
   return (
-    <div className="bg-blue-900 w-full fixed top-0 flex justify-between items-center px-2 py-3 sm:py-4 sm:px-6">
+    <div className="w-full h-[100px] fixed top-0 z-20 flex justify-between items-center px-2 py-3 sm:py-4 sm:px-6">
       {/* Sidebar */}
-        <Sidebar />
-      {/* Navbar */}
-        <div className="">
-          <img src={logo} className="w-[20rem] cursor-pointer" alt="logo" />
-        </div>
-
-        <div className="hidden sm:flex text-white gap-6">
-          <a href="#" className="opacity-100 hover:opacity-80">
-            <FaSearch size={25} />
-          </a>
-          <a href="#" className="opacity-100 hover:opacity-80">
-            <FaCartArrowDown size={25} />
-          </a>
-          <a href="#" className="opacity-100 hover:opacity-80">
-            <FaUserCircle size={25} />
-          </a>
-        </div>
+      <Sidebar />
+      <div className="hidden sm:flex sm:items-center text-white gap-8">
+        {/* Searchbar */}
+        <a href="#" className="hover:opacity-80">
+          <FaSearch size={30} />
+        </a>
+        <a href="#" className="hover:opacity-80">
+          <FaCartArrowDown size={30} />
+        </a>
+        <a href="#" className="hover:opacity-80">
+          <FaUserCircle size={30} />
+        </a>
       </div>
+    </div>
   );
 }
 
