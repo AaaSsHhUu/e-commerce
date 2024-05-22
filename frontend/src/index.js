5b52e38b891d3eacac7048509826fb35b3f92e7f
Ashu Negi <anegi6723@gmail.com>
2024-05-22 11:11:37 +0530

    added home page route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
 import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-
+        <Route path='' element={<Home />} />
     </Route>
   )
 )
