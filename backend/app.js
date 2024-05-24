fe7c365f8a6590e6ed5080e690469fa09b28fe19
Ashu Negi <anegi6723@gmail.com>
2024-05-24 10:42:30 +0530

    applied cors middleware in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
 const cookiParser = require("cookie-parser");
+const cors = require("cors");
 
+
+const corsOptions = {
+    origin : "http://localhost:3000/",
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookiParser());
+
 
 //  Routes 
 app.use("/api/v1/", require("./routes/product.route"));
