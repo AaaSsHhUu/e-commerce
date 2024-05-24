21c2db4c76175797f9dd6b6830a019d7b9784370
Ashu Negi <anegi6723@gmail.com>
2024-05-24 12:19:53 +0530

    updated result per page to 8

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -14,7 +14,7 @@ exports.createProduct = async (req, res, next) => {
 
 // Get all Products
 exports.getAllProducts = async (req, res) => {
-    const resultPerPage = 15;
+    const resultPerPage = 8;
     const productCount = await Product.countDocuments();
     const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
     let products = await apiFeature.query;
