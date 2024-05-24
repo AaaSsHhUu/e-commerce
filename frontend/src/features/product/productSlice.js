7cdd5e7621ca4993708285d813553d183f1d606a
Ashu Negi <anegi6723@gmail.com>
2024-05-24 10:44:38 +0530

    used createAsyncThunk and in productSlice.js

--- frontend/src/features/product/productSlice.js
+++ frontend/src/features/product/productSlice.js
@@ -1,19 +1,37 @@
-import  {createSlice} from "@reduxjs/toolkit";
+import  {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from 'axios';
 
-const initialState = ({
-    products : [{name : "t-shirt", price : "3000"}]
+const initialState = {
+    products : [],
+    loading : false,
+    error : null
+}
+
+export const fetchProducts = createAsyncThunk("fetchProducts", async() => {
+    const response = await axios.get("/api/v1/products");
+    console.log("products : ",response.data);
+    return response.data;
 })
 
 const productSlice = createSlice({
     name : "product",
     initialState,
-    reducers : {
-        addProduct : (state,action) => {
-            const newProduct = {name : action.payload, price : action.payload}
-            state.push(newProduct);
-        }
+    // reducers : {},
+    extraReducers : (builder) => {
+        builder.addCase(fetchProducts.pending, (state,action) => {
+            state.loading = true;
+        })
+
+        builder.addCase(fetchProducts.fulfilled, (state,action) => {
+            state.loading = false;
+            state.products = action.payload.products;
+        })
+
+        builder.addCase(fetchProducts.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
     }
 })
 
-export const {addProduct} = productSlice.actions;
 export const productReducer = productSlice.reducer;
