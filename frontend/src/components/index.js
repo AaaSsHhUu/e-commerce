612572d4e9d395054d2431c6541b584f90b7504e
Ashu Negi <anegi6723@gmail.com>
2024-05-25 18:45:03 +0530

    created index.js to export all component from one place

---
+++ frontend/src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import ProductDetails from "./Pages/Product details/ProductDetails";
+import Product from "./Products/Product";
+import Footer from "./layouts/Footer";
+import Header from "./layouts/Header";
+import Loader from "./layouts/Loader";
+import Metadata from "./layouts/Metadata";
+
+export {Navbar, Home, ProductDetails, Product, Footer, Header, Loader, Metadata}
