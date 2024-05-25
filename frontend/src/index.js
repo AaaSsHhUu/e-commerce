f525d278643bec2c397daa2b67c6fc95f06e89b4
Ashu Negi <anegi6723@gmail.com>
2024-05-25 18:45:47 +0530

    added productDetails route and modified imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
 import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from 'react-router-dom';
-import Home from './components/Pages/Home/Home';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
+import { Home, ProductDetails } from './components';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-        <Route path='' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
     </Route>
   )
 )
