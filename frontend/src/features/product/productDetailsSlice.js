eb48c903bd62834d926f9f399508ddbc3caaf4de
Ashu Negi <anegi6723@gmail.com>
2024-05-25 12:04:39 +0530

    created productDetails slice

---
+++ frontend/src/features/product/productDetailsSlice.js
@@ -0,0 +1,35 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    productDetails : {},
+    loading : false,
+    error : null
+}
+
+export const fetchProductDetails = createAsyncThunk("fetchProductDetails", async (id) => {
+    const response = await axios.get(`/api/v1/product/${id}`);
+    console.log("product details : ", response.data);
+    return response.data;
+})
+
+const productDetailsSlice = createSlice({
+    name : "productDetails",
+    initialState,
+    reducers : {},
+    extraReducers : (builder) => {
+        builder.addCase(fetchProductDetails.pending, (state,action) => {
+            state.loading = true;
+        })
+        builder.addCase(fetchProductDetails.fulfilled, (state,action) => {
+            state.loading = false;
+            state.productDetails = action.payload;
+        })
+        builder.addCase(fetchProductDetails.rejected, (state,action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+    }
+})
+
+export const productDetailsReducer =  productDetailsSlice.reducer;
