e48fc7e5b629cc20fb3e571f3b9f4c4ff4720829
Ashu Negi <anegi6723@gmail.com>
2024-05-25 10:00:16 +0530

    replaced static values with dynamic  api values

--- frontend/src/components/Products/Product.jsx
+++ frontend/src/components/Products/Product.jsx
@@ -7,18 +7,18 @@ const Product = ({ product }) => {
     edit: false,
     color: "rgba(20,20,20,0.1)",
     activeColor: "tomato",
-    value: 2.5,
+    value: product.avgRating,
     isHalf : true,
     size : window.innerWidth < 600 ? 20 : 25
   };
   return (
     <>
-      <Link className="mx-4 mb-4 w-[60vw] sm:w-[40vw] md:w-[20vw] flex flex-col border border-slate-300 text-start hover:shadow-2xl hover:-translate-y-2 transition-all duration-500" to={product.id}>
+      <Link className="mx-4 mb-4 w-[60vw] sm:w-[40vw] md:w-[20vw] flex flex-col border border-slate-300 text-start hover:shadow-2xl hover:-translate-y-2 transition-all duration-500" to={product._id}>
         <img className="mb-2" src={product.image[0].url} alt="product image" />
         <p className="text-xl my-1 mx-2">{product.name}</p>
         <div className="my-1 flex gap-2 mx-1 items-center">
           <ReactStars {...options} />
-          <span className="text-sm">(256 reviews)</span>
+          <span className="hidden sm:block text-sm">({product.numOfReviews} reviews)</span>
         </div>
         <p className="text-red-500 font-bold mx-2 my-1">₹{product.price}</p>
       </Link>
