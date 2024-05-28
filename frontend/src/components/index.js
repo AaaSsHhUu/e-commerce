49c61d45a51dfc9e675bd85bf333c1a9d15474ea
Ashu Negi <anegi6723@gmail.com>
2024-05-28 19:02:57 +0530

    exported SimpleSlider through components index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,9 +2,10 @@ import Navbar from "./Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import ProductDetails from "./Pages/Product details/ProductDetails";
 import Product from "./Products/Product";
+import SimpleSlider from "./layouts/Carousel";
 import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
 import Loader from "./layouts/Loader";
 import Metadata from "./layouts/Metadata";
 
-export {Navbar, Home, ProductDetails, Product, Footer, Header, Loader, Metadata}
+export {Navbar, Home, ProductDetails, Product, Footer, Header, Loader, Metadata, SimpleSlider};
