04551fa249b375ee9811ecb63cbc98ada7e3be60
Ashu Negi <anegi6723@gmail.com>
2024-06-02 19:29:51 +0530

    exporting banner images

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,8 @@
 import logo from './logo.png';
 import playStore from './playstore.png';
 import appStore from './Appstore.png';
+import banner1 from "./banner-1.jpg";
+import banner2 from "./banner-2.jpg";
+import banner3 from "./banner-3.jpg";
 
-export {logo, appStore, playStore};
+export {logo, appStore, playStore, banner1, banner2, banner3};
