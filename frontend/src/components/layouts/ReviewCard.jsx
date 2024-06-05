2e2d421c7022c04bd8e3019297c30142a75f7828
Ashu Negi <anegi6723@gmail.com>
2024-06-05 12:03:04 +0530

    adding styling to ReviewCard

--- frontend/src/components/layouts/ReviewCard.jsx
+++ frontend/src/components/layouts/ReviewCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
+import { profile1 } from "../../assets";
 
 const ReviewCard = ({review}) => {
   const options = {
@@ -11,10 +12,11 @@ const ReviewCard = ({review}) => {
     size: window.innerWidth < 600 ? 20 : 25,
   };
   return (
-    <div>
-        <img src="" alt="profile image" />
+    <div className="w-[30vmax] flex flex-none flex-col px-6 py-4 items-center justify-center rounded-lg hover:shadow-2xl border hover:border-gray-300 hover:scale-105">
+        <img src={profile1} alt="profile image" className="w-[50px] h-[50px] rounded-full" />
         <p>{review.name}</p>
         <ReactStars {...options} /> 
+        <span>{review.comment}</span>
     </div>
   )
 };
