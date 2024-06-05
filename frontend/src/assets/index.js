2e70c99a388e6599331f346ad3625f411f799c0c
Ashu Negi <anegi6723@gmail.com>
2024-06-05 12:02:41 +0530

    exporting profile images

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -4,5 +4,8 @@ import appStore from './Appstore.png';
 import banner1 from "./banner-1.jpg";
 import banner2 from "./banner-2.jpg";
 import banner3 from "./banner-3.jpg";
+import profile1 from "./profile-1.jpg"
+import profile2 from "./profile-2.jpg"
+import profile3 from "./profile-3.jpg"
 
-export {logo, appStore, playStore, banner1, banner2, banner3};
+export {logo, appStore, playStore, banner1, banner2, banner3, profile1, profile2, profile3};
